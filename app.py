36aad025924bb62b831d6fdecc96eaad3b1bf7a2
pavelrudenia <pavelrudeniaaa@gmail.com>
2023-03-04 21:02:04 +0300

    final commit to deploy

--- app.py
+++ app.py
@@ -249,6 +249,7 @@ def register():
 
 """http://127.0.0.1:5000/basket?csrf_token=ImVjZDExOTgzOTE5NzQ5M2YzOWNlZjI4Mjg3MGVmY2NiNzA0MTZhNzIi.YnDP1Q.2zuvpB4hCJYiHd3jCocbxOa-vKk&language=%D0%B4%D0%B8%D0%B2%D0%B0%D0%BD+%7C+%D0%A6%D0%B5%D0%BD%D0%B0+%D0%B7%D0%B0+%D1%88%D1%82.279.0&language2=xx%7C36535047&count=1
 &author=pavel"""
+@login_required
 @app.route("/basket", methods=["POST", "GET"])
 def basket():
     products = dbase.getProduct()
